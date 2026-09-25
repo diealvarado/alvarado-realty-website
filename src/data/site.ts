3eb8956be04073bf0364ea5f1a3d4a8497539489
Diego Alvarado <diego@alvaradorealtygroup.com>
2026-09-25 18:26:01 -0500

    analytics: switch GA4 data stream to G-HV8H3GDDDZ

--- src/data/site.ts
+++ src/data/site.ts
@@ -16,7 +16,7 @@ export const site = {
     full: '870 International Pkwy Ste 280, Flower Mound, TX 75022',
   },
   license: '822481',
-  gaId: 'G-XSWC5Y9YXT',
+  gaId: 'G-HV8H3GDDDZ',
   brokerage: 'Monument Realty',
   gbp: 'https://maps.app.goo.gl/RYobBs8dCmZNnQQbA?g_st=ac',
   wholesales: 'https://dfw-wholesales.netlify.app/',
